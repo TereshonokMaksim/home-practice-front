5724ad5a517772bceeeed20b0106671b31487f67
Maks Tereshonok <termaks2000@gmail.com>
2025-11-22 14:28:59 +0100

    Initialization commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    return (<div>
+        <h1>Project title</h1>
+        <p>Empty, but at least working HTML code</p>
+    </div>)
 }
-
-export default App;
